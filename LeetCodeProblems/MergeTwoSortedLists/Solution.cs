213eca5e3d6bfb0b878c72d880b23a4b46da2e8d
Gerardo Sullivan <g_j_sullivan@hotmail.com>
2018-03-28 10:34:29 +1300

    Add Run method to run test cases

--- LeetCodeProblems/MergeTwoSortedLists/Solution.cs
+++ LeetCodeProblems/MergeTwoSortedLists/Solution.cs
@@ -86,5 +86,21 @@
             }
             return dummyHead.next;
         }
+
+        /// <summary>
+        /// Run test solution
+        /// </summary>
+        public static void Run()
+        {
+            ListNode l1 = new ListNode(9);
+            l1.next = new ListNode(9);
+            //l1.next.next = new ListNode(3);
+
+            ListNode l2 = new ListNode(1);
+            //l2.next = new ListNode(9);
+            //l2.next.next = new ListNode(4);
+
+            ListNode ans = new Solution().AddTwoNumbers(l1, l2);
+        }
     }
 }
